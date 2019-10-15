0dabd2118ab837b77502601443b062f030d89736
Paul Blay <paulmblay@gmail.com>
2019-10-15 12:26:42 +0100

    starting haiku challenge, base class and readme

--- include/my_project.hpp
+++
@@ -1,4 +0,0 @@
-#ifndef MY_PROJECT
-#define MY_PROJECT
-int multiply(int x, int y);
-#endif
--- src/my_project_test.cpp
+++ src/my_project_test.cpp
@@ -1,10 +1,58 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-#include "../include/my_project.hpp"
+#include "../include/lower_case_char.hpp"
 
 
-TEST_CASE("Mutiplications are computed", "[multiply]") {
-	REQUIRE( multiply(2,3) == 6);
-	REQUIRE( multiply(1,2) == 2);
-	REQUIRE( multiply(0,5) == 0);
+TEST_CASE("Checking for vowels", "[is_a_vowel]") {
+	char test_char = 'a';
+	Lower_case_char this_char(test_char);
+	REQUIRE( this_char.is_a_vowel() == true);
+	test_char = 'e';
+	this_char.set_new_value(test_char);
+	REQUIRE( this_char.is_a_vowel() == true);
+	test_char = 'i';
+	this_char.set_new_value(test_char);
+	REQUIRE( this_char.is_a_vowel() == true);
+	test_char = 'o';
+	this_char.set_new_value(test_char);
+	REQUIRE( this_char.is_a_vowel() == true);
+	test_char = 'u';
+	this_char.set_new_value(test_char);
+	REQUIRE( this_char.is_a_vowel() == true);
+	test_char = 'y';
+	this_char.set_new_value(test_char);
+	REQUIRE( this_char.is_a_vowel() == true);
+	test_char = 'A';
+	this_char.set_new_value(test_char);
+	REQUIRE( this_char.is_a_vowel() == true);
+	test_char = 'E';
+	this_char.set_new_value(test_char);
+	REQUIRE( this_char.is_a_vowel() == true);
+	test_char = 'I';
+	this_char.set_new_value(test_char);
+	REQUIRE( this_char.is_a_vowel() == true);
+	test_char = 'O';
+	this_char.set_new_value(test_char);
+	REQUIRE( this_char.is_a_vowel() == true);
+	test_char = 'U';
+	this_char.set_new_value(test_char);
+	REQUIRE( this_char.is_a_vowel() == true);
+	test_char = 'Y';
+	this_char.set_new_value(test_char);
+	REQUIRE( this_char.is_a_vowel() == true);
+	test_char = 'b';
+	this_char.set_new_value(test_char);
+	REQUIRE( this_char.is_a_vowel() == false);
+	test_char = 'B';
+	this_char.set_new_value(test_char);
+	REQUIRE( this_char.is_a_vowel() == false);
+	test_char = ' ';
+	this_char.set_new_value(test_char);
+	REQUIRE( this_char.is_a_vowel() == false);
+	test_char = '~';
+	this_char.set_new_value(test_char);
+	REQUIRE( this_char.is_a_vowel() == false);
+	test_char = '!';
+	this_char.set_new_value(test_char);
+	REQUIRE( this_char.is_a_vowel() == false);
 }
